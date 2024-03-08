d285dd9a25bb6cb58ee01b127e86faa0e0b598d7
Neo Kung <nkung@nkung-ltmgxpb.internal.salesforce.com>
2024-03-08 14:09:25 +0800

    Section 2.15 done

--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -7,19 +7,25 @@ import (
 )
 
 // Store provides all functions to executve db queries and transactions
-type Store struct {
+type Store interface {
+	Querier
+	TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error)
+}
+
+// SQLStore provides all functions to executve db queries and transactions
+type SQLStore struct {
 	*Queries
 	db *sql.DB
 }
 
-func NewStore(db *sql.DB) *Store {
-	return &Store{
+func NewStore(db *sql.DB) Store {
+	return &SQLStore{
 		db:      db,
 		Queries: New(db),
 	}
 }
 
-func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
 
 	// tx, err := store.db.BeginTx(ctx, &sql.TxOptions{})
 	tx, err := store.db.BeginTx(ctx, nil)
@@ -60,7 +66,7 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
-func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
+func (store *SQLStore) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
